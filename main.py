63fce5710b2a48b10e0762c5db6032498e1c77c8
Zdenko Kanoš <xkanos@stuba.sk>
2024-11-20 13:03:13 +0100

    works?

---
+++ main.py
@@ -0,0 +1,23 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+# Define the transformation to convert images to tensor and normalize
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+# Download the training and test dataset
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+# Load the datasets into DataLoader
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+# Example: Iterating through the training data
+for images, labels in train_loader:
+    print(images.shape)  # Shape of the batch of images
+    print(labels.shape)  # Shape of the batch of labels
+    break
